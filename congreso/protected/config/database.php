9e30d3edefbe5c968a5f247d37f6ab10fb3e52c2
YeseniaME <yesenia-me@hotmail.com>
2015-04-15 22:10:04 -0500

    CambioConexion2

--- congreso/protected/config/database.php
+++ congreso/protected/config/database.php
@@ -5,10 +5,10 @@ return array(
 	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 	
-	'connectionString' => 'mysql:host=localhost;dbname=equipo2',
+	'connectionString' => 'mysql:host=localhost;dbname=equipo2', //citec
 	'emulatePrepare' => true,
-	'username' => 'equipo2',
-	'password' => 'yty36yt',
+	'username' => 'equipo2',  //root
+	'password' => 'yty36yt',  //''
 	'charset' => 'utf8',
 	
 );
