d33fe4dc780351f3fea8b27078459964416ffa7d
YeseniaME <yesenia-me@hotmail.com>
2015-05-06 20:17:00 -0500

    changes

--- congreso/protected/config/database.php
+++ congreso/protected/config/database.php
@@ -7,8 +7,8 @@ return array(
 	
 	'connectionString' => 'mysql:host=localhost;dbname=citec', //citec, equipo2
 	'emulatePrepare' => true,
-	'username' => 'equipo2',  //root, equipo2
-	'password' => 'yty36yt',  //'', yty36yt
+	'username' => 'root',  //root, equipo2
+	'password' => '',  //'', yty36yt
 	'charset' => 'utf8',
 	
 );
